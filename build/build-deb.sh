d5407f3dca8759c8b8a9385047ec136b524717e3
Vinod S R <smartvinu@gmail.com>
2020-05-09 21:58:49 +0530

    added emoji to the script

--- build/build-deb.sh
+++ build/build-deb.sh
@@ -1,13 +1,15 @@
+#!/bin/bash
 BASEDIR=$(dirname $0)
-echo "cleanup : $BASEDIR/../out"
+echo -e "\U1F9F9 : $(realpath $BASEDIR/../out)"
 rm $BASEDIR/../out/*
-echo "build:go"
+echo -e "\U1F527 : go build"
 go build  -o $BASEDIR/../out/shell-butler $BASEDIR/../main.go
-echo "cp: binary"
+echo -e "\U1F69A : moving binary to deb"
 #mkdir -p $BASEDIR/../package/debian/shell-butler/usr/local/bin
 mkdir -p $BASEDIR/../package/debian/shell-butler/opt/shell-butler/bin
 cp $BASEDIR/../run $BASEDIR/../package/debian/shell-butler/opt/shell-butler/bin/shell-butler-run
 cp $BASEDIR/../out/shell-butler $BASEDIR/../package/debian/shell-butler/opt/shell-butler/bin
-echo "run: dpkg-deb"
+echo -e "\U1F3C3 : running dpkg-deb"
 dpkg-deb --build $BASEDIR/../package/debian/shell-butler  $BASEDIR/../out/shell-butler.deb
-echo "done"
+echo -e "\U1F4E6 : package available in out directory"
+echo -e "\U1F603 : done"
